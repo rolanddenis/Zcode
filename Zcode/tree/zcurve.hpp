fdeb77c7d90020f769a8d75de5a06017620c4716
gouarin <loic.gouarin@gmail.com>
2017-04-07 21:57:56 +0200

    fic zcurve 3d creation

--- Zcode/tree/zcurve.hpp
+++ Zcode/tree/zcurve.hpp
@@ -8,16 +8,27 @@ constexpr std::array<node_type, 2> zcurve_impl(node_type Xbit, node_type Ybit, n
 };
 
 template<typename node_type>
-constexpr auto zcurve_impl(node_type Xbit, node_type Ybit, node_type Zbit, std::integral_constant<int, 2> const&)
+constexpr std::array<node_type, 4> zcurve_impl(node_type Xbit, node_type Ybit, node_type Zbit, std::integral_constant<int, 2> const&)
 {
-    return std::array<node_type, 4>{{0, Ybit, Xbit, static_cast<node_type>(Xbit + Ybit)}};
+    return {0, 
+            Ybit, 
+            Xbit, 
+            static_cast<node_type>(Xbit + Ybit)
+            };
 };
 
 template<typename node_type>
 constexpr std::array<node_type, 8> zcurve_impl(node_type Xbit, node_type Ybit, node_type Zbit, std::integral_constant<int, 3> const&)
 {
-    return {0, Ybit, Xbit, Xbit + Ybit,
-            Zbit, Ybit + Zbit, Xbit + Zbit, Xbit + Ybit + Zbit};
+    return {0, 
+            Ybit, 
+            Xbit, 
+            static_cast<node_type>(Xbit + Ybit),
+            Zbit, 
+            static_cast<node_type>(Ybit + Zbit),
+            static_cast<node_type>(Xbit + Zbit), 
+            static_cast<node_type>(Xbit + Ybit + Zbit)
+            };
 };
 
 template<std::size_t dim, typename node_type>
